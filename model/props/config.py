98e40835b464e3899783776d2512af96c076884a
canermercancs <caner.mercan@cs.bilkent.edu.tr>
2018-06-11 23:38:39 +0300

    configuration parameters for the network

---
+++ model/props/config.py
@@ -0,0 +1,56 @@
+"""
+author: Caner Mercan
+"""
+
+import math
+import torch
+
+# image size after convolutional layer;
+# l: image_height / image_width
+# p: padding
+# k: kernel size
+# s: stride
+convim = lambda l,p,k,s: math.ceil((l+2*p - (k-1))/s)
+
+# system level arguments
+args = { 
+    'device'        : torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), # DO NOT CHANGE THIS
+    'batch_size'    : 128,
+    'image_size'    : [28, 28, 1], # modify this based on original input image size
+    'use_decoder'   : False,
+    'scale_decoder' : 0.00005
+}
+# 1st convolutional layer parameters (2dconvolutional unit + relu function)
+convlayer_props = {
+    'in_channels'   : args['image_size'][-1],
+    'out_channels'  : 256,
+    'kernel_size'   : 9,
+    'stride'        : 1,
+    'padding'       : 0
+}
+# 1st capsule layer (primary capsule) parameters (num_unit independent 2dconvolutional units + squash function)
+capslayer_props = {
+    'in_channels'   : convlayer_props['out_channels'], 
+    'out_channels'  : 32, 
+    'num_units'     : 8, 
+    'kernel_size'   : 9, 
+    'stride'        : 2, 
+    'padding'       : 0 
+}
+# 2nd capsule layer (digit capsule) parameters (routing by agreement)
+im_size_conv = [convim(args['image_size'][i], convlayer_props['padding'], convlayer_props['kernel_size'], convlayer_props['stride']) for i in range(2)]
+im_size_caps = [convim(im_size_conv[i],       capslayer_props['padding'], capslayer_props['kernel_size'], capslayer_props['stride']) for i in range(2)]
+digilayer_props = {
+    'in_channels'   : capslayer_props['out_channels']*im_size_caps[0]*im_size_caps[1], 
+    'in_units'      : capslayer_props['num_units'], 
+    'out_channels'  : 10, 
+    'out_units'     : 16, 
+    'routing_epoch' : 3
+}
+# Decoder block parameters
+decoder_props = {
+    'in_units'      : digilayer_props['out_units'], 
+    'out_pixels'    : args['image_size'][0]*args['image_size'][1],
+    'fc1'           : 512, 
+    'fc2'           : 1024,
+}
